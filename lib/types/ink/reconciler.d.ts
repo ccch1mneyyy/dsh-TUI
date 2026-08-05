4021c87ba79b17f485eae68d62080b36bdb0888e
ccch1mneyyy <ccch1mneyyy@users.noreply.github.com>
2026-08-05 22:19:19 +0800

    build: rebuild lib cleanly (previous commit shipped a partial tsc output after npm install raced the build)

--- lib/types/ink/reconciler.d.ts
+++ lib/types/ink/reconciler.d.ts
@@ -1,3 +1,5 @@
+import createReconciler from 'react-reconciler';
+import { type DOMElement, type TextNode } from './dom.js';
 import { Dispatcher } from './events/dispatcher.js';
 export declare function getOwnerChain(fiber: unknown): string[];
 export declare function isDebugRepaintsEnabled(): boolean;
@@ -7,6 +9,6 @@ export declare function getLastYogaMs(): number;
 export declare function markCommitStart(): void;
 export declare function getLastCommitMs(): number;
 export declare function resetProfileCounters(): void;
-declare const reconciler: any;
+declare const reconciler: createReconciler.Reconciler<DOMElement, DOMElement, TextNode, DOMElement, unknown, DOMElement>;
 export default reconciler;
 //# sourceMappingURL=reconciler.d.ts.map
